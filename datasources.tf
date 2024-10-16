75fb161f2d52e99603bd66296664d117201e94ca
Wanderson Vinhal <157320712+wandersonvinhal@users.noreply.github.com>
2024-10-16 10:59:54 -0300

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -1,20 +1,3 @@
-#IMAGEM A SER UTILIZADA NA EC2
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["amazon"]
-}
-
 data "aws_route53_zone" "existing_zone" {
   name         = "__R53_DOMAIN_NAME__"
   private_zone = false
